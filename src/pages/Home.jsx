31da28001163a01df7c7a7797bb6359702a11cdf
Guillaume <guillaumegozillon@gmail.com>
2021-09-29 03:38:33 +0200

    init react architecture

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <main>
+      <h1>Hello</h1>
+      <input type='text' name='' id='' />
+      <input type='text' name='' id='' />
+      <input type='text' name='' id='' />
+      <input type='text' name='' id='' />
+    </main>
+  )
+}
+
+export default Home
